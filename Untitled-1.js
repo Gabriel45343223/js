7ad37de74063f1f5346dd482f8d83971578be478
Gabriel45343223 <106820064+Gabriel45343223@users.noreply.github.com>
2023-10-10 09:02:40 -0300

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,15 @@
+<selecton buton class="teclado">
+<buton class="tecla tecla_pom">Pom</buton>
+<buton class="tecla tecla_clap">Clap</buton>
+<buton class="tecla tecla_tim">Tim</buton>
+
+<buton class="tecla tecla_puff">Puff</buton>
+<buton class="tecla tecla_splash">Splash</buton>
+<buton class="tecla tecla_toim">Toim</buton>
+
+<buton class="tecla tecla_psh">Psh</buton>
+<buton class="tecla tecla_tic">Tic</buton>
+<buton class="tecla tecla_tom">Tom</buton>
+</section>
+</body>
+</html>
